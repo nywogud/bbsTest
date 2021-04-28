ad699385ca49f5d7066dc2f6017b874adff64565
nywogud <nywogud@hanmail.net>
2021-04-28 18:02:45 +0900

    로그인 예외 처리

---
+++ workspace/bbs/src/main/java/com/jhl/dao/MemberDAO.java
@@ -0,0 +1,15 @@
+package com.jhl.dao;
+
+import java.util.List;
+
+import com.jhl.dto.MemberVO;
+
+public interface MemberDAO {
+	
+	public List<MemberVO> selelctMember() throws Exception;
+	
+	public int isIdExist(String id) throws Exception;
+	
+	public List<MemberVO> selectOne(String id) throws Exception;
+
+}
